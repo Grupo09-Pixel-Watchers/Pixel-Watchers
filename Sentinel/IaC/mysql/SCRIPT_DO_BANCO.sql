56618795961827c015ed7db541b570bd5601a58e
GuiScarabelli <guilherme.quadros@sptech.school>
2023-11-28 20:16:07 -0300

    Adicionando as dependencias novas no pom.xml e bibliotecas na pasta lib

--- Sentinel/IaC/mysql/SCRIPT_DO_BANCO.sql
+++ Sentinel/IaC/mysql/SCRIPT_DO_BANCO.sql
@@ -40,8 +40,8 @@ CREATE TABLE tbComputador(
                              apelidoPc VARCHAR(100),
                              sistemaOperacional VARCHAR(30),
                              processador VARCHAR(100),
-                             discoTotal LONG,
-                             memoriaTotal LONG,
+                             discoTotal VARCHAR(50),
+                             memoriaTotal VARCHAR(50),
                              qtdDiscos INT,
                              fkArena INT,
                              FOREIGN KEY (fkArena) REFERENCES tbArena (idArena)
@@ -49,9 +49,9 @@ CREATE TABLE tbComputador(
 
 CREATE TABLE status_pc(
                           idCaptura INT PRIMARY KEY AUTO_INCREMENT,
-                          memoriaUso LONG,
+                          memoriaUso DOUBLE,
                           processadorUso DOUBLE,
-                          discoDisponivel LONG,
+                          discoDisponivel DOUBLE,
                           tempProcessador DOUBLE,
                           dtHoraCaptura DATETIME DEFAULT now(),
                           fkComputador VARCHAR(100),
@@ -80,7 +80,7 @@ CREATE TABLE PastasProibidas (
 );
 
 -- Função para buscar o ID da empresa de acordo com o CNPJ
-/*DELIMITER $$
+DELIMITER $$
 CREATE FUNCTION fn_empresa(fnCnpj char(18))
     RETURNS int
     deterministic
@@ -90,7 +90,7 @@ BEGIN
 return(vId);
 END$$;
 DELIMITER ;
-*/
+
 -- INSERT INTO tbusuario (nome, sobrenome, email, senha)VALUES ('MC', 'Lovin','gui@gmail.com', '123');
 
 /*insert into tbComputador values (null, 'Windows 11','Intel Core i7 7700k','1000','16', 1, 1),
@@ -152,3 +152,9 @@ INSERT INTO pastasProibidas (nomePasta, motivoProibicao) VALUES
     ('Windows API', 'Possível ferramenta de modificação de sistema'),
     ('ArtMoney', 'Uso indevido de cheats'),
     ('Cheat Engine', 'Uso indevido de cheats');*/
+
+
+/*  insert into tbEmpresa values (null, 'Empresa', 'RazaoSocial', '1231230912391', '(11)92372-0293', 60);
+    insert into tbUsuario values (null, 'Kaua', 'Juhrs', 'a@gmail.com', 'a', 'admin', 1);
+    insert into tbArena values (null, 'asd', '02932-000', 'Rua 1', '11', 'jd Flores', 'São Paulo', 'SP', 1);
+*/
